78e0132728f0a03efee4782feb105b3590409062
Lina Ma <lina.ma0616@gmail.com>
2020-12-15 03:41:10 -0700

    completed ipynd

--- SF Salary 2011/query.sql
+++ SF Salary 2011/query.sql
@@ -1,23 +1,37 @@
--- List first name, last name, and hire date for employees who were hired in 1986.
+-- List EmployeeName, JobTitle, TotalPayBenefits and Year for SF Major.
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle like 'MAYOR';
 
--- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a chief level job title
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
-WHERE JobTitle LIKE '%MANAGER%';
+WHERE JobTitle LIKE '%CHIEF%';
 
--- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+-- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a senior level job title
+SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
+WHERE JobTitle LIKE '%SENIOR%';
+
+-- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a junior level job title
+SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
+WHERE JobTitle LIKE '%JUNIOR%';
+
+
+-- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that are assistans:
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle LIKE '%ASSISTANT';
 
--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- In descending order, list the average total pay wth benefits by employees.
 SELECT EmployeeName, ROUND(AVG(TotalPayBenefits), 0) AS TotalPay
 FROM salaries 
 GROUP BY EmployeeName
 ORDER BY TotalPay DESC;
 
--- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- In descending order, list the average total pay wth benefits by job titles.
 SELECT JobTitle, ROUND(AVG(TotalPayBenefits), 0) AS TotalPay
 FROM salaries
 GROUP BY JobTitle
 ORDER BY TotalPay DESC;
+
+-- Find if there's any data related job in city of san francisco 
+SELECT * FROM Salaries 
+WHERE LOWER(JobTitle) LIKE '%data%'
+ORDER BY JobTitle;
