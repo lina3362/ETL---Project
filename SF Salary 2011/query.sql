3e16f3b7f12b3fc6f55f7687e65e93f285605228
Lina Ma <lina.ma0616@gmail.com>
2020-12-15 03:48:24 -0700

    query

--- SF Salary 2011/query.sql
+++ SF Salary 2011/query.sql
@@ -5,19 +5,43 @@ WHERE JobTitle like 'MAYOR';
 -- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a chief level job title
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle LIKE '%CHIEF%';
+-- See what the average total salary for chief level
+SELECT JobTitle, ROUND(AVG(TotalPayBenefits), 0) AS Avg_TotalPay
+FROM salaries
+WHERE JobTitle LIKE '%CHIEF%'
+GROUP BY JobTitle
+ORDER BY Avg_TotalPay DESC;
 
 -- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a senior level job title
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle LIKE '%SENIOR%';
+-- See what the average total salary for senior level
+SELECT JobTitle, ROUND(AVG(TotalPayBenefits), 0) AS Avg_TotalPay
+FROM salaries
+WHERE JobTitle LIKE '%SENIOR%'
+GROUP BY JobTitle
+ORDER BY Avg_TotalPay DESC;
 
 -- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that have a junior level job title
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle LIKE '%JUNIOR%';
+-- See what the average total salary for junior level
+SELECT JobTitle, ROUND(AVG(TotalPayBenefits), 0) AS Avg_TotalPay
+FROM salaries
+WHERE JobTitle LIKE '%JUNIOR%'
+GROUP BY JobTitle
+ORDER BY Avg_TotalPay DESC;
 
 
 -- List mployeeName, JobTitle, TotalPayBenefits and Year for employees that are assistans:
 SELECT EmployeeName, JobTitle, TotalPayBenefits, Year FROM salaries
 WHERE JobTitle LIKE '%ASSISTANT';
+-- See what the average total salary for assistant type of job
+SELECT JobTitle, ROUND(AVG(TotalPayBenefits), 0) AS Avg_TotalPay
+FROM salaries
+WHERE JobTitle LIKE '%ASSISTANT'
+GROUP BY JobTitle
+ORDER BY Avg_TotalPay DESC;
 
 -- In descending order, list the average total pay wth benefits by employees.
 SELECT EmployeeName, ROUND(AVG(TotalPayBenefits), 0) AS TotalPay
